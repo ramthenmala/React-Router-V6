4950a3a45dd910fbf4c01620bec713b9bd456e2c
Ram kumar <webbieram@outlook.com>
2022-07-25 17:34:17 +0530

    dynamic routes

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,16 @@
+import { Link } from 'react-router-dom';
+import productList from '../data';
+
 const Products = () => {
   return (
     <>
-      <section className='section'>
+      <section className="section">
         <h2>products</h2>
+        {productList.map((product) => (
+          <Link to={product.id} key={product.id}>
+            <div>{product.name}</div>
+          </Link>
+        ))}
       </section>
     </>
   );
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,14 +1,16 @@
 import { Link, useParams } from "react-router-dom";
+import products from "../data";
 
 const SingleProduct = () => {
   const { productId } = useParams();
   console.log('SingleProduct', productId);
+  const product = products.find(product => product.id === productId);
+  const {name} = product
   return (
-    <section className='section product'>
-      <Link to='/products'>Back to products</Link>
+    <section className="section product">
+      <Link to="/products">Back to products</Link>
 
-      <h2>single product {productId}</h2>
-
+      <h2>single product {name}</h2>
     </section>
   );
 };
